4be9388fdeaa58173104740b07f42aa2b763d63b
Prasad Deshpande <57430207+prasad981@users.noreply.github.com>
2021-08-17 16:20:55 +0530

    Add files via upload

---
+++ RelexAssignment/src/main/java/com/Relex/productproject/RelexAssignment/RelexAssignmentApplication.java
@@ -0,0 +1,15 @@
+package com.Relex.productproject.RelexAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelexAssignmentApplication {
+
+	public static void main(String[] args) {
+	//	SpringApplication.run(RelexAssignmentApplication.class, args);
+	System.out.println("hello spring");
+	}
+	
+
+}
---
+++ RelexAssignment/src/test/java/com/Relex/productproject/RelexAssignment/RelexAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Relex.productproject.RelexAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelexAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
